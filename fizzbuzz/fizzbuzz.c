9961f5bed4acc0bf8415797f6b00efbae03de436
Addison-Stavlo <AddieStavlo@gmail.com>
2019-04-22 17:21:28 -0400

    fizzbuzz complete

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,27 @@
 */
 int fizzbuzz(int n)
 {
-
+    int i = 0;
+    for (int j = 0; j < n; j++)
+    {
+        if (j % 3 == 0 && j % 5 == 0)
+        {
+            printf("FizzBuzz\n");
+        }
+        else if (j % 3 == 0)
+        {
+            printf("Fizz\n");
+        }
+        else if (j % 5 == 0)
+        {
+            printf("Buzz\n");
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return i;
 }
 
 #ifndef TESTING
