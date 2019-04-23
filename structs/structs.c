ce05deb6883becd53fce3c79df92ab8182c70834
Addison-Stavlo <AddieStavlo@gmail.com>
2019-04-23 15:59:06 -0400

    createPerson working

--- structs/structs.c
+++ structs/structs.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lib.h"
-
+#include "malloc.h"
 /* 
     Define the Person struct by specifying the fields that make up the
     Person type. Don't forget to specify the type of each field. A 
     Person should have the fields `name`, `age`, `height`, and `weight`.
 */
-typedef struct Person {
-
+typedef struct Person
+{
+    char *name;
+    int age;
+    int height;
+    int weight;
 } Person;
 
 /*
@@ -21,7 +25,13 @@ typedef struct Person {
 */
 Person *createPerson(char *name, int age, int height, int weight)
 {
+    Person *person = malloc(sizeof(Person));
+    person->name = string_dup(name);
+    person->age = age;
+    person->height = height;
+    person->weight = weight;
 
+    return person;
 }
 
 /*
@@ -30,7 +40,6 @@ Person *createPerson(char *name, int age, int height, int weight)
 */
 void destroyPerson(Person *who)
 {
-
 }
 
 #ifndef TESTING
